15a2546bb5b7ad10a3a9c79143523ac51093430e
Feyruz Yalcin <fy2@sanger.ac.uk>
2012-11-01 09:48:46 +0000

    take out the css and js dir

--- core/js/developer/toggle.js
+++
@@ -1,4 +0,0 @@
-$('body').prepend('<span class="dev-toggle devpanel">hide dev</span><span class="dev-toggle devpanel" style="display:none">show dev</span>');
-$('.dev-toggle').live('click', function () {
-  $('.devpanel').toggle();
-});
