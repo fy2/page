035c8f31c5bab3cf5411fe2d201f861887e0aa2b
Feyruz Yalcin <fy2@sanger.ac.uk>
2012-11-13 09:45:15 +0000

    simple authentication in all /data/* sections active

--- PageApp/lib/PageApp/Controller/Data.pm
+++ PageApp/lib/PageApp/Controller/Data.pm
@@ -24,9 +24,17 @@ Catalyst Controller.
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
     
-    #$c->response->redirect($c->uri_for(
-     #           $c->controller('Login')->action_for('data')));
-                
+    # If a user doesn't exist, force login
+#    if (!$c->user_exists) {
+        # Dump a log message to the development server debug output
+#        $c->log->debug('***Root::auto User not found, forwarding to /login');
+        # Redirect the user to the login page
+#        $c->response->redirect($c->uri_for('/login'));
+        # Return 0 to cancel 'post-auto' processing and prevent use of application
+#        return 0;
+#    }
+          
+               
     $c->stash( active_action => '/data' );
 
     $c->stash->{template} = 'data.tt2';
@@ -78,6 +86,37 @@ sub resources :Path('resources'){
     $c->stash( active_action => '/data/resources' );
     
     $c->stash->{template} = 'resources.tt2';
+}
+
+
+=head2 auto
+
+Check if there is a user and, if not, forward to login page
+
+=cut
+
+# Note that 'auto' runs after 'begin' but before your actions and that
+# 'auto' "chain" (all from application path to most specific class are run)
+sub auto : Private {
+    my ($self, $c) = @_;
+
+    # Allow unauthenticated users to reach the login page
+    if ($c->request->path =~ /login/) {
+        return 1;
+    }
+
+    # If a user doesn't exist, force login
+    if (!$c->user_exists) {
+        # Dump a log message to the development server debug output
+        $c->log->debug('***Root::auto User not found, forwarding to /login');
+        # Redirect the user to the login page
+        $c->response->redirect($c->uri_for('/login'));
+        # Return 0 to cancel 'post-auto' processing and prevent use of application
+        return 0;
+    }
+
+    # User found, so return 1 to continue with processing after this 'auto'
+    return 1;
 }
 
 =head1 AUTHOR
