2eefa422e812186683c516f447935847ebfd7344
Feyruz Yalcin <fy2@sanger.ac.uk>
2013-04-19 17:09:24 +0100

    allow max 10 act comparisons. make crunch.gz suffix in pageapp.yaml

--- PageApp/root/static/page/js/artemis_act_checks.js
+++ PageApp/root/static/page/js/artemis_act_checks.js
@@ -22,8 +22,8 @@ function act_args_are_okay (selectedData) {
 	
 	var sanger_id_array = selectedData.split("checkbox=");
 	sanger_id_array.shift(); //first element is empty
-	if (sanger_id_array.length < 2 || sanger_id_array.length > 5) {
-		msg.innerHTML='<div class="msg">Too few or too many genomes selected. Please select at least 2 (max = 5) for Act.</div>';
+	if (sanger_id_array.length < 2 || sanger_id_array.length > 10) {
+		msg.innerHTML='<div class="msg">Too few or too many genomes selected. Please select at least 2 (max = 10) for Act.</div>';
 		window.setTimeout(function(){msg.innerHTML='';},3000);
 		return false;
 	}
