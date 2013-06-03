4c554889395f614853d49095b5e5b46c6ffbe155
Feyruz Yalcin <fy2@sanger.ac.uk>
2013-06-03 08:44:48 +0100

    update sftp instruction doc

--- PageApp/root/database/set_hashed_passwords.pl
+++ PageApp/root/database/set_hashed_passwords.pl
@@ -2,14 +2,14 @@
  
 use strict;
 use warnings;
-use lib './lib';
+use lib '../../lib';
 use PageApp::Schema;
  
 my $schema = PageApp::Schema->connect('dbi:SQLite:pageapp.db');
  
 my @users = $schema->resultset('User')->all;
 foreach my $user (@users) {
-    next unless ($user->username eq 'guest');
+#    next unless ($user->username eq 'everett');
     my $existing_password = $user->get_column('password');
     $user->password($existing_password); #will implicitly hash and salt it (see User.pm)
     $user->update; #commit
