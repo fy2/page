8036917b09bad78c4a18378a9b8cff40c29906bc
Feyruz Yalcin <fy2@sanger.ac.uk>
2013-08-17 16:33:37 +0100

    start seems to work

--- utilities/starman.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-$PAGEPERLLIB/bin/starman -I$PAGEAPPDIR/lib -I$PAGEPERLLIB/lib/perl5 $PAGEPSGIAPP --workers $PAGEWORKERS --pid $PAGEPIDFILE --port $PAGEPORT --daemonize --error-log $PAGELOGDIR/error.log --access-log $PAGELOGDIR/access.log
-
